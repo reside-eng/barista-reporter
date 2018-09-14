7bf7fef0f9e732df9a37baea1a9416116032670c
Scott <prescottprue@users.noreply.github.com>
2018-09-13 23:54:54 -0700

    v0.1.0
    * fix(reporter): remove invalid usage of firebaseInstance.auth

--- src/firebaseUtils.js
+++ src/firebaseUtils.js
@@ -1,31 +1,15 @@
-/* eslint-disable no-console */
 import firebase from 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
+import { stageFbConfig, prodFbConfig } from './constants';
 
 let firebaseInstance;
-
-const stageFbConfig = {
-  apiKey: 'AIzaSyD8UB1rOfw5oWzwyKrKvH0WLJ6wDPC94ac',
-  authDomain: 'barista-stage.firebaseapp.com',
-  databaseURL: 'https://barista-stage.firebaseio.com',
-  projectId: 'barista-stage',
-  storageBucket: 'barista-stage.appspot.com',
-  messagingSenderId: '109344700598',
-};
-
-const prodFbConfig = {
-  apiKey: 'AIzaSyCiaUr9jIU_FdTKArOE0UsZq3K-ftChbLg',
-  authDomain: 'barista-836b4.firebaseapp.com',
-  databaseURL: 'https://barista-836b4.firebaseio.com',
-  projectId: 'barista-836b4',
-  storageBucket: 'barista-836b4.appspot.com',
-  messagingSenderId: '438807155877',
-};
 
 /**
  * Initialize Firebase instance from service account (from local
  * serviceAccount.json)
+ * @param {Object} reporterOptions - Options passed to the reporter
+ * @param {Boolean} reporterOptions.useStage - Whether or not to use Barista stage environment
  * @return {Firebase} Initialized Firebase instance
  */
 export function initializeFirebase({ useStage }) {
@@ -37,19 +21,30 @@ export function initializeFirebase({ useStage }) {
     }
     return firebaseInstance;
   } catch (err) {
-    console.log('Error initializing firebase instance from service account.');
+    console.log('Error initializing firebase instance from service account.'); // eslint-disable-line no-console
     throw err;
   }
 }
 
+/**
+ * Authenticate anonymously with Firebase
+ */
 export function authWithFirebase() {
+  // Check to see if user is already authed
+  if (firebase.auth().currentUser) {
+    return Promise.resolve(firebase.auth().currentUser);
+  }
+
   return new Promise((resolve, reject) => {
-    firebaseInstance.auth().onAuthStateChanged(user => {
+    // Attach auth state change listener that resolves promise after login
+    firebase.auth().onAuthStateChanged(user => {
       if (user) {
         resolve(user);
       }
     });
-    firebaseInstance
+
+    // Authenticate anonymously rejecting on failure
+    firebase
       .auth()
       .signInAnonymously()
       .catch(error => {
@@ -69,9 +64,11 @@ export function writeToDatabase(dbRef, data) {
     return authWithFirebase().then(() => writeToDatabase(dbRef, data));
   }
   return dbRef.update(data).catch(err => {
+    /* eslint-disable no-console */
     console.log(
-      `error writing test run data to Firebase at path: ${dbRef.path}`,
+      `Error writing test run data to Firebase at path: ${dbRef.path}`,
     );
+    /* eslint-enable no-console */
     return Promise.reject(err);
   });
 }
--- test/unit/index.spec.js
+++ test/unit/index.spec.js
@@ -1,9 +1,26 @@
-import src from '../../src';
+import BaristaReporter from 'index';
+import mocha from 'mocha';
 
 describe('barista-reporter module', () => {
   describe('exports', () => {
-    it('BaristaReporter', () => {
-      expect(src).to.exist;
+    it('BaristaReporter function', () => {
+      expect(BaristaReporter).to.be.a('function');
+    });
+  });
+
+  describe('extends', () => {
+    let entendStub;
+    before(() => {
+      entendStub = sinon.stub(mocha.reporters, 'Base');
+    });
+
+    // Skipped since test never exits. Check mocha reporter
+    // tests here: https://github.com/mochajs/mocha/blob/master/test/reporters/json.spec.js
+    it.skip('mocha reporters', () => {
+      const onStub = sinon.stub();
+      const fakeRunner = { on: onStub };
+      BaristaReporter(fakeRunner);
+      expect(entendStub).to.be.calledOnce;
     });
   });
 });
